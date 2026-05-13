e00d2813727d373c0db6472dbc44c49eb383aaba
mohdayan0306-web <mohdayan0306@gmail.com>
2026-05-14 01:35:27 +0530

    Initial commit

---
+++ React+Ts app/src/App.tsx
@@ -0,0 +1,33 @@
+ 
+
+
+function App() {
+      
+  return (
+    <div>
+      
+const header = (
+  <header>
+    <h1>Mozilla Developer Network</h1>
+  </header>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
+
